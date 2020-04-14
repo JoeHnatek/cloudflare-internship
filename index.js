5693599682206e02d8eadb74345460b135b6c66f
Joseph Hnatek <jhnatek07@gmail.com>
2020-04-14 18:00:45 -0500

    Added functionality of HTMLRewriter

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
+class elementHandler {
+  constructor(elementName) {
+    this.elementName = elementName
+  }
+
+  element(element) {
+
+    //Changes the title of the web page
+    if(element.tagName === 'title'){
+      element.setInnerContent('Go Bulldogs!')
+    }
+
+    //Changes main title of the page
+    if(element.tagName === 'h1' && element.getAttribute('id') === 'title'){
+      element.setInnerContent('Joseph Hnatek')
+    }
+
+    //Changes the description paragraph
+    if(element.tagName === 'p' && element.getAttribute('id') === 'description'){
+      element.setInnerContent('Double Majoring in Computer Science and Cognitive Science at UMN - Duluth. Check out my Github while you are here!')
+    }
+
+    //Changes call to action link and text
+    if(element.tagName === 'a' && element.getAttribute('id') === 'url'){
+      const item = element.getAttribute('href')
+      element.setAttribute('href', item.replace("https://cloudflare.com", "https://github.com/jmh07"));
+      element.setInnerContent('Click to visit github.com/jmh07')
+    }
+  }
+}
+
+const rewriter = new HTMLRewriter()
+  .on('*', new elementHandler())
+
 
 /**
  * Respond with hello worker text
@@ -27,8 +58,7 @@ async function handleRequest(request) {
 
   let resultOfRandomURL;
 
-  console.log(typeof randomURL)
-  await fetch(randomURL, {
+  /*await fetch(randomURL, {
     method: 'GET',
     headers: { 'content-type': 'text/html; charset=UTF-8' }
   })
@@ -36,10 +66,17 @@ async function handleRequest(request) {
   .then(data => {
     resultOfRandomURL = data
     console.log(data)
-  })
+  })*/
 
+  const res = await fetch('https://cfw-takehome.developers.workers.dev/variants/2')
 
-  return new Response(resultOfRandomURL, {
+  //let a = rewriter.transform(res);
+  /*return new Response(a, {
     headers: { 'content-type': 'text/html; charset=UTF-8' },
-  })
+  })*/
+  return rewriter.transform(res)
 }
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
